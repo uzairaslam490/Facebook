d6ca34542a493ecd5bf77d641b22d336736b2896
unknown <uzairaslam490@gmail.com>
2022-04-11 16:11:38 +0500

    Facebook

---
+++ resources/views/createpost.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.default')
+
+@section('page-content')
+<div class="container mt-4" style="min-height: 533px;">
+    <div class="row">
+        <div class="offset-lg-1 col-lg-10">
+        @if(session()->has('post_success'))
+            <div class="mb-4">
+                <div class="alert alert-success form-control">
+                    {{session()->get('post_success')}}
+                </div>
+            </div>
+        @endif
+            <h2>Create a post</h2>
+            <form action="{{route('addpost', ['id' => 1])}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <div class="custom-file">
+                        <input class="custom-file-input" type="File" name="image" id="ImageSelect" value="">
+                        <label for="ImageSelect" class="custom-file-label">Select Image</label>
+                    </div>
+                </div>
+                <textarea name="post" id="post" cols="30" rows="10" class="form-control" placeholder="Something on your mind..."></textarea>
+                <button type="Submit" name="Submit" class="btn btn-primary form-control" style="float: left">Post</button>
+            </form>
+            <br>
+            
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+
+@section('page-content')
+<div class="container mt-4" style="min-height: 533px;">
+    <div class="row">
+        <div class="offset-lg-1 col-lg-10">
+
+           <h2> Fetched Posts Goes here </h2>
+            
+        </div>
+    </div>
+@endsection
